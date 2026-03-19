872fb76dd6db0af9c037b5808fa96b7ed7308d9e
Big Dahn <harkinborholarholuwah@gmail.com>
2026-03-19 11:56:51 +0100

    fix: email queue

--- Queues/emailQueue.ts
+++ Queues/emailQueue.ts
@@ -5,15 +5,19 @@ if (process.env.NODE_ENV !== 'production') {
 
 import { Queue } from "bullmq"
 
+const connection = {
+    host: process.env.REDIS_HOST ?? "localhost",
+    port: Number(process.env.REDIS_PORT) || 6379,
+    password: process.env.REDIS_PASSWORD || undefined,
+    tls: process.env.NODE_ENV === 'production' ? {} : undefined
+}
 
-const emailQueue =process.env.NODE_ENV === "test" ? null :  new Queue("emailQueue", {
-    connection: {
-        host: process.env.REDIS_HOST ?? "localhost",
-        port: Number(process.env.REDIS_PORT) || 6379,
-        password: process.env.REDIS_PASSWORD || undefined,
-        tls: process.env.NODE_ENV === 'production' ? {} : undefined
 
-    }
+const emailQueue = new Queue("emailQueue", { 
+    connection: process.env.NODE_ENV === 'test' ? { host: 'localhost', port: 6379 } : connection
 })
+
+
+
 
 export default emailQueue
