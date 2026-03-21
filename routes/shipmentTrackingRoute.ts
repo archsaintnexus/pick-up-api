f436fad79dd0f74bcae53eb398305287d3ca4f70
onose <ogbidionose@gmail.com>
2026-03-21 22:29:40 +0100

    fix: eslint no-unused-vars

--- routes/shipmentTrackingRoute.ts
+++ routes/shipmentTrackingRoute.ts
@@ -1,9 +1,7 @@
-import { Router } from "express";
 import validator from "../middleware/validator.js";
 import { getPOD, getTimeline, getTracking, updateStatus, uploadPOD } from "../controller/shipmentTrackingController.js";
 import { validatePOD, validateTrackingEvent } from "../SchemaTypes/shipmentTrackingSchema.js";
 import express from "express";
-import { createTracking } from "../controller/shipmentTrackingController.js";
 
 const router = express.Router();
 
